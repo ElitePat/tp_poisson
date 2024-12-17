037307c92241e2feb5ab93e41ec6733bee03b04d
nsundi_mboli <patrick.nsundi_mboli@etu.sorbonne-universite.fr>
2024-12-17 21:10:03 +0100

    Test de la fonction BLAS dgbmv

--- src/tp_env.c
+++ src/tp_env.c
@@ -4,13 +4,43 @@
 /* test the environment of compilation    */
 /******************************************/
 #include "tp_env.h"
+#define N 4 // taille matrices (pour l'instant carrées)
+
 int main(int argc,char *argv[])
 /* ** argc: Nombre d'arguments */
 /* ** argv: Valeur des arguments */
 {
 	printf("--------- Test environment of execution for Practical exercises of Numerical Algorithmics ---------\n\n");
 
-	double *GB;
+	printf("Test de la méthode dgbmv de BLAS\n");
+	double alpha=1 ,beta=2; // scalaires
+	double *GB, *x, *y; // matrice poisson 1D en GB et vecteurs
+
+	// phase d'allocation mémoire
+	x = (double *) malloc(sizeof(double)*N);
+	y = (double *) malloc(sizeof(double)*N);
+	GB = (double *) malloc(sizeof(double)*N*N);
+
+	set_GB_operator_colMajor_poisson1D(GB, 3, N, 1);
+	for(int i=0,i<N;i++){
+		x[i] = 0;
+		y[i] = 0;
+	} // pour l'instant on fait simple
+
+	// appel a dgbmv qui faira le calcul suivant: y := alpha*GB*x + beta*y
+	dgbmv('N',N,N,1,1,alpha,GB,N,x,1,beta,y,1);
+	// c'est le vecteur y qui contient le resultat de l'appel
+
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			printf(" %ld",GB[N*i+j]);
+		}
+		printf("\n");
+	}
+
+	free(x);
+	free(y);
+	free(GB);
 	
 
 	/*
